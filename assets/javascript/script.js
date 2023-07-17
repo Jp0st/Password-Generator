41fcb81048e12222ef29efcfda343849d30d9353
Jpost <Jpost2010@Gmail.com>
2023-07-17 15:53:30 -0400

    Added 4 functions to the HTML to prompt the user on whether they would like to include uppercase,lowercase,numbers,or special characters in the generation. Temporary console outputs to confirm functionality as well

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,61 @@
 // Assignment code here
 
+var test = "";
+
+function askUpperCase(){
+  var ifUpperCase = confirm("Would you like to include uppercase letters?");
+  if(ifUpperCase){
+    test = "yes";
+    console.log(test +" uppercase");
+  }else{
+    test = "no";
+    console.log(test +" uppercase");
+  }
+}
+
+function askLowerCase(){
+  var ifLowerCase = confirm("Would you like to include lowercase letters?");
+  if(ifLowerCase){
+    test = "yes";
+    console.log(test +" lowercase");
+  }else{
+    test = "no";
+    console.log(test +" lowercase");
+  }
+}
+
+function askNumber(){
+  var ifNumber = confirm("Would you like to include numbers?");
+  if(ifNumber){
+    test = "yes";
+    console.log(test +" number");
+  }else{
+    test = "no";
+    console.log(test +" number");
+  }
+}
+
+function askSpecial(){
+  var ifSpecial = confirm("Would you like to include special characters?");
+  if(ifSpecial){
+    test = "yes";
+    console.log(test +" characters");
+  }else{
+    test = "no";
+    console.log(test +" characters");
+  }
+}
+
+
+function generatePassword(){
+  askUpperCase();
+  askLowerCase();
+  askNumber();
+  askSpecial();
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +71,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
